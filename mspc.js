5590d53d2b96658160643fce34f307f96fbc6612
littledove-hue <lildovee758@gmail.com>
2026-03-18 17:04:19 +0530

    Update mspc.js

--- mspc.js
+++ mspc.js
@@ -26,7 +26,7 @@ const scripts = [
   { name: 'Boyfriend Kiss', fn: runBoyfriendKiss, alwaysRun: true },
   { name: 'Guild Show', fn: runGuildShow, alwaysRun: true },
   { name: 'Pet Training', fn: runPetTraining, alwaysRun: false },
-  { name: 'Bridesmaids Tasks', fn: runBridesmaids, alwaysRun: false },
+  { name: 'Bridesmaids Tasks', fn: runBridesmaids, alwaysRun: true },
 ];
 
 (async () => {
